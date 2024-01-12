2d12de4301214f8fbb90e73fd9e0dae0505dbf53
Karyna <karyna.sanjaya@gmail.com>
2024-01-12 08:21:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Savoria_Remake
+ Remake of my team's final project on Visual Programming
---
+++ app/src/main/java/com/example/savoria_remake/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.savoria_remake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.savoria_remake.ui.theme.Savoria_RemakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Savoria_RemakeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Savoria_RemakeTheme {
+        Greeting("Android")
+    }
+}
